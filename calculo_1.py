38fd381cc6d9110e45fc505a107dd6aa59e2ddfc
Paulahrndez <paula.hdz@hotmail.com>
2024-06-12 15:14:18 +0200

    Calculo_1 sin cambios

---
+++ calculo_1.py
@@ -0,0 +1,10 @@
+b=30
+c=30*10*5
+
+print('resultado=',c)
+
+## Cambiando el factor 102 según requerimiento del negocio
+## Cambiando el factor 110 según rJunta de Control
+
+d=c*110
+print('resultado total=',d)
