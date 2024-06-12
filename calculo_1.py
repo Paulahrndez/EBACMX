b2d0c829df59e3a11b67c4182ea350b387c1bd3e
Paulahrndez <paula.hdz@hotmail.com>
2024-06-12 15:26:27 +0200

    Factor 200

--- calculo_1.py
+++ calculo_1.py
@@ -4,7 +4,9 @@ c=30*10*5
 print('resultado=',c)
 
 ## Cambiando el factor 102 según requerimiento del negocio
-## Cambiando el factor 110 según rJunta de Control
+## Cambiando el factor 110 según Junta Control
+## cambio a factor 200 por Directorio 
 
-d=c*110
+d=c*200
+
 print('resultado total=',d)
